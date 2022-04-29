bf374312fedd0e1571295586d681d4f5ed6cc7eb
ARUNKARTHICK782 <arunkarthick.cs20@bitsathy.ac.in>
2022-04-29 13:24:38 +0530

    commit 1

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+          home: loginpage(),
+    );
+  }
+}
+
+class loginpage extends StatefulWidget {
+  const loginpage({Key? key}) : super(key: key);
+
+  @override
+  State<loginpage> createState() => _loginpageState();
+}
+
+class _loginpageState extends State<loginpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
+
+
