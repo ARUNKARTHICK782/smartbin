ec7b70b06d828bf2c1fdc289ab1c152703b766ba
ARUNKARTHICK782 <arunkarthick.cs20@bitsathy.ac.in>
2022-04-30 12:58:30 +0530

    commit 4

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:smartbin/screens/regions.dart';
-
 class homepage extends StatefulWidget {
   const homepage({Key? key}) : super(key: key);
 
